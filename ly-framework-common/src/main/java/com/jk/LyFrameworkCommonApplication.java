0bc2af90070533d57134cc63e6c35e79af5a59c6
Your Name <email@example.com>
2019-05-10 19:36:14 +0800

    第一次提交common

---
+++ ly-framework-common/src/main/java/com/jk/LyFrameworkCommonApplication.java
@@ -0,0 +1,13 @@
+package com.jk;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LyFrameworkCommonApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LyFrameworkCommonApplication.class, args);
+	}
+
+}
